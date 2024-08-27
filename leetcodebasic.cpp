a211346732365c5f1876f3de42245276cdd1ee6b
shreeteja22 <smutukundu2206@gmail.com>
2024-08-27 19:07:05 +0530

    dsa_cpp

---
+++ leetcodebasic.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> num_map;
+    
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        
+        if (num_map.find(complement) != num_map.end()) {
+            return {num_map[complement], i};
+        }
+        
+        num_map[nums[i]] = i;
+    }
+    
+    return {};  // In case no solution is found, though the problem guarantees one.
+}
+
+int main() {
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    std::vector<int> result = twoSum(nums, target);
+    
+    if (!result.empty()) {
+        std::cout << "[" << result[0] << ", " << result[1] << "]" << std::endl;
+    } else {
+        std::cout << "No solution found!" << std::endl;
+    }
+    
+    return 0;
+}
